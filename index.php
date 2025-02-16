34d25d415cc84e3fcc3a530173fe4c5883fc4c96
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-02-16 16:54:39 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio expedita voluptatibus rem illo ad ab itaque enim in amet? Porro possimus nostrum, officia nesciunt exercitationem explicabo id officiis quidem fugiat!</p>
 </body>
 
 </html>
